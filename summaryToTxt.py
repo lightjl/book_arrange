de63808506a2bd6b5068e3e7e8f406d96e6f96e5
bg <bg@example.com>
2017-05-11 21:49:17 +0800

    fixed bugs

--- summaryToTxt.py
+++ summaryToTxt.py
@@ -2,54 +2,105 @@ import getContent
 import pandas as pd
 import arrange
 from collections import Counter
+import math
 
 class Summary:
-    def __init__(self, fold, summaryPath):
+    def __init__(self, fold, zPath, ePath):
         self.toTxt = getContent.saveToFile(fold)
-        self.dfBook = pd.read_excel(summaryPath)
-        self.ar = arrange.ar(summaryPath, '')
+        self.zBook = pd.read_excel(zPath)
+        self.eBook = pd.read_excel(ePath)
+        self.zr = arrange.ar(zPath, '')
+        self.er = arrange.ar(ePath, '')
         self.tagSet = set()
         self.tagList = []
         pass
 
+    def __fixFileName(self, fileName):
+        fileName = fileName.replace('：', '__')
+        fileName = fileName.replace(':', '__')
+        fileName = fileName.replace('/', '_')
+        fileName = fileName.replace('*', '')
+        fileName = fileName.replace('\\', '')
+        fileName = fileName.replace('<', '')
+        fileName = fileName.replace('>', '')
+        fileName = fileName.replace('|', '')
+        return fileName
+
     def saveTxt(self):
-        for (index, row) in self.dfBook.iterrows():
+        for (index, row) in self.zBook.iterrows():
             #print(type(list(row[2])))
             listTag = []
-            #print(row[2])
-            #print(type(row[2])==float)
             if not(row[2] == '[]' or type(row[2])==float):
                 listTag = row[2].split("['")[1].split("']")[0].split("', '")
             for tag in listTag:
                 self.tagList.append(tag)
-        print(Counter(self.tagList).most_common(32))
+
+        for (index, row) in self.eBook.iterrows():
+            if row[0] in self.zBook['name'].values:
+                continue
+            #print(type(list(row[2])))
+            listTag = []
+            if not(row[2] == '[]' or type(row[2])==float):
+                listTag = row[2].split("['")[1].split("']")[0].split("', '")
+            for tag in listTag:
+                self.tagList.append(tag)
+
+        print(Counter(self.tagList).most_common(64))
         mostTag64 = [ith[0] for ith in Counter(self.tagList).most_common(64)]
         print(mostTag64)
-        for (index, row) in self.dfBook.iterrows():
+        for (index, row) in self.zBook.iterrows():
             fileName = row[0]
             fileName = fileName.replace('[', '')
             #print(type(list(row[2])))
             listTag = []
             #print(row[2])
             #print(type(row[2])==float)
+            fileName += '['
             if not(row[2] == '[]' or type(row[2])==float):
                 listTag = row[2].split("['")[1].split("']")[0].split("', '")
-                fileName += '['
                 for tag in listTag:
-                    if(tag in mostTag64):
-                        fileName += tag + ' '
-            fileName += tag + ']'
-            fileName = fileName.replace('：', '__')
-            fileName = fileName.replace(':', '__')
-            fileName = fileName.replace('/', '_')
-            fileName = fileName.replace('*', '')
+                    fileName += tag + ' '
+            fileName += 'zBook '
+            #print(type(row[3]) == float and row[3] != 0 and (not math.isnan(row[3])))
+            if (type(row[3]) == float and row[3] != 0 and (not math.isnan(row[3]))):
+                fileName += str(int(row[3]/2+1))+'star]'
+            else:
+                fileName += ']'
+            fileName = self.__fixFileName(fileName)
             print(fileName+'-'+str(row[3]))
             #print(row[4])      #url
             #print(row[5])
             self.toTxt.save(fileName+'-'+str(row[3]), str(row[4])+'\r\n'+str(row[5]))
 
-        pass
+
+        for (index, row) in self.eBook.iterrows():
+            if row[0] in self.zBook['name'].values:
+                continue
+            fileName = row[0]
+            fileName = fileName.replace('[', '')
+            fileName = fileName.replace(']', '-')
+            #print(type(list(row[2])))
+            listTag = []
+            #print(row[2])
+            #print(type(row[2])==float)
+            fileName += '['
+            if not(row[2] == '[]' or type(row[2])==float):
+                listTag = row[2].split("['")[1].split("']")[0].split("', '")
+                for tag in listTag:
+                    fileName += tag + ' '
+            fileName += 'eBook '
+            if (type(row[3]) == float and row[3] != 0 and (not math.isnan(row[3]))):
+                fileName += str(int(row[3]/2+1))+'star]'
+            else:
+                fileName += ']'
+
+            fileName = self.__fixFileName(fileName)
+            print(fileName+'-'+str(row[3]))
+            #print(row[4])      #url
+            #print(row[5])
+            self.toTxt.save(fileName+'-'+str(row[3]), str(row[4])+'\r\n'+str(row[5]))
 
 
-zBook = Summary('eBook', r'D:/pythonResouce/ebookAR.xls')
+
+zBook = Summary('ezBook', r'D:/pythonResouce/zbook.xls', r'D:/pythonResouce/ebookAR.xls')
 zBook.saveTxt()
